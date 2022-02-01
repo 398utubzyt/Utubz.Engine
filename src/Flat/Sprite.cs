33942874015e47e91e22b54e5628256dd0b606d1
398utubzyt <398utubzyt@gmail.com>
2022-01-31 20:24:15 -0800

    begin flat module

---
+++ src/Flat/Sprite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utubz.Flat
+{
+    public sealed class Sprite : Object
+    {
+    }
+}
